c1670ef26662ff89288c39a9222b20c37bf96ad0
Roman Romanko <rroma5129@gmail.com>
2023-08-22 00:14:43 +0300

    products section has been completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,27 @@
 `use strict`;
 
-const productItem = document.querySelector(".product__item");
-const productItemHovered = document.querySelector(".product__item-hovered");
+const productItems = document.querySelectorAll(".product__item");
+let previousItem;
 
-productItem.addEventListener("click", () => {
-  productItemHovered.classList.toggle("hide");
+productItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (previousItem !== undefined && previousItem !== item) {
+      const previousItemHiden = previousItem.querySelector(
+        ".product__item-hovered"
+      );
+
+      if (!previousItemHiden.classList.contains("hide")) {
+        previousItemHiden.classList.add("hide");
+      }
+      if (previousItem.classList.contains("product__item-sale--hover")) {
+        previousItem.classList.remove("product__item-sale--hover");
+      }
+      previousItem = item;
+    } else if (previousItem === undefined) {
+      previousItem = item;
+    }
+    const hoveredItem = item.querySelector(".product__item-hovered");
+    hoveredItem.classList.toggle("hide");
+    item.classList.toggle("product__item-sale--hover");
+  });
 });
