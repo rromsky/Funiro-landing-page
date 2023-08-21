b0a403515f00059d9cfdfcc6be141d5012c68f3d
Roman Romanko <rroma5129@gmail.com>
2023-08-21 16:19:45 +0300

    first commit

---
+++ scripts/lazy-load.js
@@ -0,0 +1,43 @@
+const targets = document.querySelectorAll(".lazy-hide");
+
+const lazyLoad = (target) => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const t = entry.target;
+        t.classList.remove("lazy-hide");
+
+        observer.disconnect();
+      }
+    });
+  });
+
+  io.observe(target);
+};
+
+targets.forEach(lazyLoad);
+
+const images = document.querySelectorAll(".lazy-image");
+
+const lazyLoadImage = (target) => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const t = entry.target;
+        const src = t.getAttribute("data-lazy");
+        t.setAttribute("src", src);
+        t.classList.remove("lazy-image");
+        observer.disconnect();
+      }
+    });
+  });
+
+  io.observe(target);
+};
+
+images.forEach(lazyLoadImage);
+
+const bac = document.querySelector(".photo-desctription-container");
+bac.addEventListener("hover", () => {
+  console.log("hovered");
+});
