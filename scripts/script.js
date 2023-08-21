f7cb95c6be4b30994f0b44544e68d014bfeaaeb9
Roman Romanko <rroma5129@gmail.com>
2023-08-21 20:35:07 +0300

    added  hovered part of products items

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 `use strict`;
+
+const productItem = document.querySelector(".product__item");
+const productItemHovered = document.querySelector(".product__item-hovered");
+
+productItem.addEventListener("click", () => {
+  productItemHovered.classList.toggle("hide");
+});
