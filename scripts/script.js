18a7f4c8a78e182cdfcf288e0f94fb48d67e407e
Roman Romanko <rroma5129@gmail.com>
2023-08-23 02:59:26 +0300

    The adaptive layout is finished

--- scripts/script.js
+++ scripts/script.js
@@ -28,4 +28,84 @@ productItems.forEach((item) => {
     item.classList.toggle("product__item-sale--hover");
   });
 });
-console.log(window.innerWidth);
+
+let heroItem = 1;
+let normalize = 20;
+
+const heroNextItem = element(".hero-btn.slider__btn--right");
+const heroPrevItem = element(".hero-btn.slider__btn--left");
+const heroSetItem = document.querySelectorAll("hero__indicator");
+
+heroNextItem.addEventListener(`click`, () => {
+  if (heroItem < 3) {
+    document.querySelectorAll(".hero__photo-desctription").forEach((item) => {
+      item.setAttribute(
+        "style",
+        `transform: translateX(${heroItem * -100 - normalize}%)`
+      );
+    });
+    element(`.photo-desctription-container.id${heroItem}`).classList.toggle(
+      `hide`
+    );
+    element(`.hero__indicator.id${heroItem}`).classList.toggle("active");
+    heroItem++;
+    element(`.photo-desctription-container.id${heroItem}`).classList.toggle(
+      `hide`
+    );
+    element(`.hero__indicator.id${heroItem}`).classList.toggle("active");
+  } else {
+    element(`.hero__indicator.id${heroItem}`).classList.toggle("active");
+    element(`.photo-desctription-container.id${heroItem}`).classList.toggle(
+      `hide`
+    );
+    heroItem = 1;
+    element(`.photo-desctription-container.id${heroItem}`).classList.toggle(
+      `hide`
+    );
+    element(`.hero__indicator.id${heroItem}`).classList.toggle("active");
+
+    document.querySelectorAll(".hero__photo-desctription").forEach((item) => {
+      item.setAttribute(
+        "style",
+        `transform: translateX(${(heroItem - 1) * -100 - normalize}%)`
+      );
+    });
+  }
+});
+heroPrevItem.addEventListener(`click`, () => {
+  if (heroItem > 1) {
+    document.querySelectorAll(".hero__photo-desctription").forEach((item) => {
+      item.setAttribute(
+        "style",
+        `transform: translateX(${(heroItem - 2) * -100 - normalize}%)`
+      );
+    });
+
+    element(`.hero__indicator.id${heroItem}`).classList.toggle("active");
+    element(`.photo-desctription-container.id${heroItem}`).classList.toggle(
+      `hide`
+    );
+    heroItem--;
+    element(`.hero__indicator.id${heroItem}`).classList.toggle("active");
+    element(`.photo-desctription-container.id${heroItem}`).classList.toggle(
+      `hide`
+    );
+  } else {
+    element(`.hero__indicator.id${heroItem}`).classList.toggle("active");
+    element(`.photo-desctription-container.id${heroItem}`).classList.toggle(
+      `hide`
+    );
+    heroItem = 3;
+    element(`.hero__indicator.id${heroItem}`).classList.toggle("active");
+    element(`.photo-desctription-container.id${heroItem}`).classList.toggle(
+      `hide`
+    );
+
+    document.querySelectorAll(".hero__photo-desctription").forEach((item) => {
+      item.setAttribute(
+        "style",
+        `transform: translateX(${(heroItem - 1) * -100 - normalize}%)`
+      );
+    });
+  }
+});
