c00426e8bd703a8d18152b8f6b938ad9ce4fb8e5
Roman Romanko <rroma5129@gmail.com>
2023-08-23 05:19:31 +0300

    Animation almost done

--- scripts/script.js
+++ scripts/script.js
@@ -34,7 +34,7 @@ let normalize = 20;
 
 const heroNextItem = element(".hero-btn.slider__btn--right");
 const heroPrevItem = element(".hero-btn.slider__btn--left");
-const heroSetItem = document.querySelectorAll("hero__indicator");
+const heroSetItem = document.querySelectorAll(".hero__indicator");
 
 heroNextItem.addEventListener(`click`, () => {
   if (heroItem < 3) {
@@ -109,7 +109,27 @@ heroPrevItem.addEventListener(`click`, () => {
     });
   }
 });
-
+// heroSetItem.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     element(`.photo-desctription-container.id${heroItem}`).classList.toggle(
+//       `hide`
+//     );
+//     element(`.hero__indicator.id${heroItem}`).classList.toggle("active");
+//     heroItem = item.getAttribute("idd") - 0;
+//     element(`.photo-desctription-container.id${heroItem}`).classList.toggle(
+//       `hide`
+//     );
+//     element(`.hero__indicator.id${heroItem}`).classList.toggle("active");
+//     console.log(`${heroItem}`);
+//     console.log(`.hero__indicator.id${heroItem}`);
+//     document.querySelectorAll(".hero__photo-desctription").forEach((el) => {
+//       el.setAttribute(
+//         "style",
+//         `transform: translateX(${heroItem * -100 - normalize}%)`
+//       );
+//     });
+//   });
+// });
 //MOBILE NAVIGATION OPEN/CLOSE
 
 const mobileOpenBtn = element(".mobile-nav--open");
@@ -121,3 +141,59 @@ const swapNav = () => {
 };
 mobileOpenBtn.addEventListener(`click`, swapNav);
 mobileNav.addEventListener(`click`, swapNav);
+
+//Tips & Tricks slide bar
+
+const nextTipItem = document.getElementById(`pls`);
+const prevTipItem = element(".tips-btn.slider__btn--left");
+let tipSlide = 2;
+
+nextTipItem.addEventListener("click", () => {
+  element(`.slide.id${tipSlide}`).classList.toggle(`active`);
+  element(`.tips__indicator.id${tipSlide}`).classList.toggle(`active`);
+  tipSlide = tipSlide == 5 ? 1 : tipSlide + 1;
+  element(`.slide.id${tipSlide}`).classList.toggle(`active`);
+  element(`.tips__indicator.id${tipSlide}`).classList.toggle(`active`);
+  console.log(tipSlide);
+  for (let i = 1; i <= 5; i++) {
+    element(`.slide.id${i}`).setAttribute(
+      "style",
+      `transform: translateX(${i * 105 + (tipSlide - 1) * -100}%)`
+    );
+  }
+});
+prevTipItem.addEventListener("click", () => {
+  element(`.tips__indicator.id${tipSlide}`).classList.toggle(`active`);
+  element(`.slide.id${tipSlide}`).classList.toggle(`active`);
+  tipSlide = tipSlide == 1 ? 5 : tipSlide - 1;
+  element(`.slide.id${tipSlide}`).classList.toggle(`active`);
+  element(`.tips__indicator.id${tipSlide}`).classList.toggle(`active`);
+  console.log(tipSlide);
+  for (let i = 1; i <= 5; i++) {
+    element(`.slide.id${i}`).setAttribute(
+      "style",
+      `transform: translateX(${i * 105 + (tipSlide - 1) * -100}%)`
+    );
+  }
+});
+
+//Rooms slide bar
+const nextRoom = element(`.slider__btn.rooms-btn`);
+const rooms = [1, 2, 3];
+
+nextRoom.addEventListener(`click`, () => {
+  element(`.rooms__indicator.id${rooms[0]}`).classList.toggle(`active`);
+  rooms.push(rooms[0]);
+  rooms.reverse();
+  rooms.pop();
+  rooms.reverse();
+  element(`.rooms__indicator.id${rooms[0]}`).classList.toggle(`active`);
+  for (let i = 1; i <= 3; i++) {
+    element(`.photo__section-element.item--id-${rooms[i - 1]}`).classList.add(
+      `item${i}`
+    );
+    element(
+      `.photo__section-element.item--id-${rooms[i - 1]}`
+    ).classList.remove(`item${i == 3 ? 1 : i + 1}`);
+  }
+});
